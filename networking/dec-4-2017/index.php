5fd1f33fb545695a27aeb5cb3a0cf48a24fce325
Daniel Lim <hbt@markhamboards.com>
2017-11-13 23:31:15 -0500

    update Dec 5 event page
    update Dec 5 event page

---
+++ networking/dec-4-2017/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">  
+    <title>Battleford Corp. (Coming Soon)</title>
+    <style>@import url(/assets/style.css);</style>  
+    <link rel="stylesheet" href="css/style.css">  
+		<!-- Facebook Pixel Code -->
+		<script>
+		  !function(f,b,e,v,n,t,s)
+		  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+		  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+		  n.queue=[];t=b.createElement(e);t.async=!0;
+		  t.src=v;s=b.getElementsByTagName(e)[0];
+		  s.parentNode.insertBefore(t,s)}(window, document,'script',
+		  'https://connect.facebook.net/en_US/fbevents.js');
+		  fbq('init', '487884434923825');
+		  fbq('track', 'PageView');
+		</script>
+		<noscript><img height="1" width="1" style="display:none"
+		  src="https://www.facebook.com/tr?id=487884434923825&ev=PageView&noscript=1"
+		/></noscript>
+		<!-- End Facebook Pixel Code -->
+</head>
+<?php flush(); ?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-108355750-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-108355750-1');
+</script>
+
+<body>  
+<script>!function(a,b,c,d,t){var e,f=a.getElementsByTagName("head")[0];if(!a.getElementById(c)){if(e=a.createElement(b),e.id=c,e.setAttribute("data-vrlps-ucid",d),e.setAttribute("data-vrlps-version","2"), e.setAttribute("data-vrlps-template", t),e.src="https://app.viral-loops.com/popup_assets/js/vl_load_v2.min.js",window.ub){jQuery=null,$=null;var g=a.createElement(b);g.src="https://code.jquery.com/jquery-2.2.4.min.js",f.appendChild(g)}f.appendChild(e);var h=a.createElement("link");h.rel="stylesheet",h.type="text/css",h.href="https://app.viral-loops.com/static/vl-loader.css",f.appendChild(h);var i=a.createElement("div");i.id="vl-overlay",i.style.display="none";var j=a.createElement("div");j.id="vl-loader",i.appendChild(j),a.addEventListener("DOMContentLoaded",function(b){a.body.appendChild(i);for(var c=a.getElementsByClassName("vrlps-trigger"),d=0;d<c.length;d++)c[d].removeAttribute("href"),c[d].onclick=function(){a.getElementById("vl-overlay").style.display="block"};var e=a.querySelectorAll("[data-vl-widget='popupTrigger']");[].forEach.call(e,function(b){var c=a.createElement("div");c.className="vl-embedded-cta-loader",b.appendChild(c)})})}}(document,"script","vrlps-js","AZ-xuzVVjsuKFsDU0Zd6zlRH_xo","waitlist");</script>  
+  <div id=container>
+    <br />
+	<?php /*
+    <a href="#" class="vrlps-trigger">  
+      <img src="./img/battlefordcorp-logo.png" id="avatar" width="500" draggable="false"/>
+    </a>
+	*/ ?>
+	
+    <a href="#">  
+		<img src="./img/battlefordcorp-logo.png" id="avatar" width="500" draggable="false"/>
+	</a>	
+    <div id=text>
+      <br />
+        <h1>Battleford Corporation Inc.</h1>
+        <p title="(Coming Soon!)">(Coming Soon)</p>    
+      <br />
+        <div data-vl-widget="popupTrigger"></div>
+    </div>
+  </div>
+</body>
+</html>
---
+++ networking/dec-4-2017/source-README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/seanfree/pen/WZaXVR.
+
+A canvas experiment using typed arrays and offscreen drawing to get a more efficient glow effect for large numbers of particles.
